93aa19ca19314f2713e47d2fbce51b4d1f5ed902
techchee <ketyung@techchee.com>
2023-04-17 08:52:06 +0800

    update

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,5 @@
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-
-pub fn calculate_hash<T: Hash>(t: &T) -> u64 {
-    let mut s = DefaultHasher::new();
-    t.hash(&mut s);
-    s.finish()
-}
-
-
-pub fn hash_to_hex<T:Hash>(t: &T) -> String {
-
-    let h = calculate_hash(&t);
-    format!("{:x}", h)
-}
+use std::hash::Hash;
+use pix0_market_handlers::utils::hash_to_hex;
 
 
 pub fn nft_token_id<T:Hash>(t: &T) -> String {
